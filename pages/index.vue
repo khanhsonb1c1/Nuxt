d4a6bdede44ad3d5c520ea395ccf1008ddfaade2
khanhsonb1c1 <khanhsonb1c1@gmail.com>
2023-04-02 11:42:03 +0700

    fetch api

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,22 @@
 <template>
-  <div>
-    <h1>Home page</h1>
+  <div class="container mx-auto">
+    <div class="home-page">
+      <div class="card" v-for="(product, index) in products" :key="index">
+        <h1>{{product.title}}</h1>
+      </div>
+    </div>
   </div>
 </template>
 
-<script>
+<script setup>
+
+const {data: products} = await useFetch('https://fakestoreapi.com/products') 
+
 </script>
 
+<style lang="scss" scoped>
+.home-page {
+  min-height: 100vh;
+  background: #3333;
+}
+</style>
